b8309b45db5b6d39d927c6a10c018790d569afe3
Stanislaw Sikora <remixy0@onet.pl>
2025-10-27 11:49:08 +0100

    cwiczenia

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    Data data = new Data();
+    Data data = new Data(12,1,2024);
+    System.out.println(data.toString());
+    data.ustawDate(12,12,12);
     System.out.println(data.toString());
     }
 }
