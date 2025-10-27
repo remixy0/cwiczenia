b6e78a23fef1e8c0b34dbe9d163f80e5abc2bbf4
Stanislaw Sikora <remixy0@onet.pl>
2025-10-27 11:42:38 +0100

    cwiczenia

--- src/Data.java
+++ src/Data.java
@@ -3,6 +3,7 @@ public class Data {
     int miesiac;
     int rok;
 
+
     public Data(){
         this.dzien = 27;
         this.miesiac = 10;
@@ -29,4 +30,27 @@ public class Data {
     public void setDzien(int dzien) {
         this.dzien = dzien;
     }
+    public String toString(){
+        return String.valueOf(dzien) + "." + String.valueOf(miesiac) + "." + String.valueOf(rok);
+    }
+    public void ustawDate(int dzien, int miesiac, int rok) {
+        this.dzien = dzien;
+        this.miesiac = miesiac;
+        this.rok = rok;
+    }
+
+    public boolean czyPrzestepny(){
+        if(rok % 4 == 0 && rok % 100 == 0 && rok % 400 == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean czyPoprawna(){
+        if (dzien <= 31 && dzien > 0 && miesiac <= 12 &&  miesiac > 0){
+            return true;
+        }else return false;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+    Data data = new Data();
+    System.out.println(data.toString());
     }
 }
